86742968625a147a5a43e97b465315373b72dcc7
Francesco Pontiggia <francesco.pontiggia@mail.polimi.it>
2022-06-14 16:33:08 +0200

    refactor sketch.py and pc_property.py to fit the pc experiment

--- paynt/paynt/sketch/pc_property.py
+++ paynt/paynt/sketch/pc_property.py
@@ -7,6 +7,14 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+# Probabilistic conformance formula
+# overall formula: ES sh . A s1 . E s2 .
+#     (P(F die1(s1)) = P(F die1(s2)))
+#   & (P(F die2(s1)) = P(F die2(s2)))
+#   & (P(F die3(s1)) = P(F die3(s2)))
+#   & (P(F die4(s1)) = P(F die4(s2)))
+#   & (P(F die5(s1)) = P(F die5(s2)))
+#   & (P(F die6(s1)) = P(F die6(s2)))
 
 class PC_Property:
     # model checking precision
@@ -16,19 +24,20 @@ class PC_Property:
 
     ''' Wrapper over a stormpy property. '''
 
-    def __init__(self, prop_left, prop_right):
+    def __init__(self, prop):
+
+        #the state variables of this property
+        self.L_state_var = "s1"
+        self.R_state_var = "s2"
 
         # each property specifies an equality between two probabilities
-        self.L_property = prop_left
-        self.R_property = prop_right
+        self.property = prop
         self.op = operator.eq
 
-        self.L_formula_str = prop_left.raw_formula
-        self.R_formula_str = prop_right.raw_formula
-
+        self.formula = prop.raw_formula
 
     def __str__(self):
-        return str(self.R_formula_str) + " = " + str(self.R_formula_str)
+        return str(self.formula) + " = " + str(self.formula)
 
     def meets_op(self, a, b):
         return self.op(a, b)
@@ -36,11 +45,17 @@ class PC_Property:
     def satisfies_threshold(self, value_left, value_right):
         return self.meets_op(value_left, value_right)
 
-#a specification is just a set of properties
+# a specification is just
+#    - a set of properties (i.e. all the equalities)
+#    - the dictionary to retrive the state quantifications
+
+
 class Specification:
 
     def __init__(self, constraints):
         self.constraints = constraints
+        # see the formula for the probabilistic conformance
+        self.sq_dict = {"s1" : "AS", "s2" : "ES"}
 
     def __str__(self):
         constraints = ",".join([str(c) for c in self.constraints])
@@ -50,13 +65,18 @@ class Specification:
         return [i for i, _ in enumerate(self.constraints)]
 
     def stormpy_properties(self):
-        properties = [c.property for c in self.constraints]
+        properties = [c.L_property for c in self.constraints] + [c.R_property for c in self.constraints]
         return properties
 
     def stormpy_formulae(self):
-        mc_formulae = [c.formula for c in self.constraints]
+        mc_formulae = [c.L_formula for c in self.constraints] + [c.R_formula for c in self.constraints]
         return mc_formulae
 
+    @classmethod
+    def string_formulae(cls):
+        fs = ["P=? [F \"die1\"", "P=? [F \"die2\"", "P=? [F \"die3\"",
+              "P=? [F \"die4\"", "P=? [F \"die5\"", "P=? [F \"die6\""]
+        return fs
 
 # TODO: refactor this as well (I have no idea whether we need it at the moment)
 
--- paynt/paynt/sketch/sketch.py
+++ paynt/paynt/sketch/sketch.py
@@ -1,6 +1,6 @@
 import stormpy
 
-from .property import Property, OptimalityProperty, Specification
+from .pc_property import PC_Property, Specification, ThresholdSpecification, DPSpecification
 from .holes import Hole, Holes, DesignSpace
 from ..synthesizers.models import MarkovChain
 from ..synthesizers.quotient import *
@@ -8,6 +8,7 @@ from ..profiler import Profiler
 
 import os
 import re
+import operator
 import uuid
 
 import logging
@@ -15,17 +16,11 @@ logger = logging.getLogger(__name__)
 
 class Sketch:
 
-    # if True, unfolded JANI is exported and the program is aborted
-    export_jani = False
-    # if True, the sketch is assumed to be a hole-free MDP
-    hyperproperty_synthesis = False
-
-    def __init__(self, sketch_path, properties_path, constant_str):
+    def __init__(self, sketch_path):
 
         Profiler.initialize()
         Profiler.start("sketch")
-        
-        self.explicit_model = None
+
         self.prism = None
         self.hole_expressions = None
 
@@ -34,163 +29,51 @@ class Sketch:
         self.quotient = None
 
         constant_map = None
-        expression_parser = None
 
         logger.info(f"Loading sketch from {sketch_path}...")
-        logger.info(f"Attempting to parse model in explicit format ...")
-        self.explicit_model = Sketch.read_explicit_model(sketch_path)
+        logger.info(f"Assuming a sketch in a PRISM format ...")
+        self.prism, hole_definitions = Sketch.load_sketch(sketch_path)  # TODO: but for an hyperproperty there are no holes, hole defitnions will be an empty dictionary
 
-        if self.is_explicit:
-            logger.info(f"Successfully parsed model in explicit format.")
-            self.design_space = DesignSpace()
+        expression_parser = stormpy.storage.ExpressionParser(self.prism.expression_manager)
+        expression_parser.set_identifier_mapping(dict())
+
+        # constants that are undefined in the sketch and are not holes.
+        if constant_str != '':
+            logger.info(f"Assuming constant definitions: '{constant_str}' ...")
+            constant_map = Sketch.map_constants(self.prism, expression_parser, constant_str)
+            self.prism = self.prism.define_constants(constant_map)
+            self.prism = self.prism.substitute_constants()
+
+        if len(hole_definitions) == 0:
+            logger.info("Sketch does not contain any hole definitions.")
+            self.design_space = DesignSpace() # TODO: hyperproperties -> MDP without holes -> how to deal with this?
         else:
-            logger.info(f"Assuming a sketch in a PRISM format ...")
-            self.prism, hole_definitions = Sketch.load_sketch(sketch_path)
+            logger.info("Processing hole definitions ...")
+            self.prism, self.hole_expressions, self.design_space = Sketch.parse_holes(self.prism, expression_parser, hole_definitions)
 
-            expression_parser = stormpy.storage.ExpressionParser(self.prism.expression_manager)
-            expression_parser.set_identifier_mapping(dict())
-
-            if constant_str != '':
-                logger.info(f"Assuming constant definitions: '{constant_str}' ...")
-                constant_map = Sketch.map_constants(self.prism, expression_parser, constant_str)
-                self.prism = self.prism.define_constants(constant_map)
-                self.prism = self.prism.substitute_constants()
-
-            if len(hole_definitions) == 0:
-                logger.info("Sketch does not contain any hole definitions.")
-                self.design_space = DesignSpace()
-            else:
-                logger.info("Processing hole definitions ...")
-                self.prism, self.hole_expressions, self.design_space = Sketch.parse_holes(self.prism, expression_parser, hole_definitions)
-                
-                logger.info(f"Sketch has {self.design_space.num_holes} holes")
-                # logger.info(f"Listing hole domains: {holes}"
-                logger.info(f"Design space size: {self.design_space.size}")
+            logger.info(f"Sketch has {self.design_space.num_holes} holes")
+            # logger.info(f"Listing hole domains: {holes}"
+            logger.info(f"Design space size: {self.design_space.size}")
 
         logger.info(f"Loading properties from {properties_path} ...")
         self.specification = Sketch.parse_specification(self.prism, constant_map, properties_path)
         logger.info(f"Found the following specification: {self.specification}")
         self.design_space.property_indices = self.specification.all_constraint_indices()
         MarkovChain.initialize(self.specification.stormpy_formulae())
-        
-        logger.info(f"Initializing the quotient ...")
 
-        if self.is_dtmc:
-            self.quotient = DTMCQuotientContainer(self)
-        elif self.is_ma:
-            self.quotient = MAQuotientContainer(self)
-        elif self.is_mdp:
-            if Sketch.hyperproperty_synthesis:
-                self.quotient = HyperPropertyQuotientContainer(self)
-            else:
-                self.quotient = MDPQuotientContainer(self)
-        elif self.is_pomdp:
-            self.quotient = POMDPQuotientContainer(self)
-            self.quotient.pomdp_manager.set_memory_size(POMDPQuotientContainer.pomdp_memory_size)
-            self.quotient.unfold_memory()
-        else:
-            raise TypeError("sketch type is not supported")
+        logger.info(f"Initializing the quotient ...")
+        self.quotient = HyperPropertyQuotientContainer(self)
 
         logger.info(f"Sketch parsing complete.")
         Profiler.stop()
 
-
-    @property
-    def is_explicit(self):
-        return self.explicit_model is not None
-
-    @property
-    def is_dtmc(self):
-        return not self.is_explicit and self.prism.model_type == stormpy.storage.PrismModelType.DTMC
-    
-    @property
-    def is_ctmc(self):
-        return not self.is_explicit and self.prism.model_type == stormpy.storage.PrismModelType.CTMC
-
-    @property
-    def is_ma(self):
-        return not self.is_explicit and self.prism.model_type == stormpy.storage.PrismModelType.MA
-
-    @property
-    def is_mdp(self):
-        return not self.is_explicit and self.prism.model_type == stormpy.storage.PrismModelType.MDP
-
-    @property
-    def is_pomdp(self):
-        if self.is_explicit:
-            return self.explicit_model.is_nondeterministic_model and self.explicit_model.is_partially_observable
-        else:
-            return self.prism.model_type == stormpy.storage.PrismModelType.POMDP
-
-    @classmethod
-    def read_explicit_model(cls, path):
-        explicit_model = None
-        try:
-            builder_options = stormpy.core.DirectEncodingParserOptions()
-            builder_options.build_choice_labels = True
-            explicit_model = stormpy.core._build_sparse_model_from_drn(path, builder_options)
-        except:
-            pass
-        return explicit_model
-
-
+    # load a PRISM model into a PRISM representation with stormpy method parse_prism_program
     @classmethod
     def load_sketch(cls, sketch_path):
-        # read lines
-        with open(sketch_path) as f:
-            sketch_lines = f.readlines()
-
-        # replace hole definitions with constants
-        hole_re = re.compile(r'^hole\s+(.*?)\s+(.*?)\s+in\s+\{(.*?)\};$')
-        sketch_output = []
-        hole_definitions = {}
-        for line in sketch_lines:
-            match = hole_re.search(line)
-            if match is not None:
-                hole_type = match.group(1)
-                hole_name = match.group(2)
-                hole_options = match.group(3).replace(" ", "")
-                hole_definitions[hole_name] = hole_options
-                line = f"const {hole_type} {hole_name};"
-            sketch_output.append(line)
-
-        # store modified sketch to a temporary file
-        tmp_path = sketch_path + str(uuid.uuid4())
-        with open(tmp_path, 'w') as f:
-            for line in sketch_output:
-                print(line, end="", file=f)
-
-        # try to parse temporary sketch and then delete it
-        try:
-            prism = stormpy.parse_prism_program(tmp_path, prism_compat=True)
-            os.remove(tmp_path)
-        except:
-            os.remove(tmp_path)
-
+        # todo: add parsing of actions as holes
+        hole_definitions = None
+        prism = stormpy.parse_prism_program(sketch_path, prism_compat=True)
         return prism, hole_definitions
-
- 
-    @classmethod
-    def map_constants(cls, prism, expression_parser, constant_str):
-        constant_str = constant_str.replace(" ", "")
-        if constant_str == "":
-            return dict()
-        
-        constant_map = dict()
-        constant_definitions = constant_str.split(",")
-        prism_constants = {c.name:c for c in prism.constants}
-
-        for constant_definition in constant_definitions:
-            key_value = constant_definition.split("=")
-            if len(key_value) != 2:
-                raise ValueError(f"Expected key=value pair, got '{constant_definition}'.")
-
-            expr = expression_parser.parse(key_value[1])
-            name = key_value[0]
-            constant = prism_constants[name].expression_variable
-            constant_map[constant] = expr
-        return constant_map
-
 
     @classmethod
     def parse_holes(cls, prism, expression_parser, hole_definitions):
@@ -228,81 +111,21 @@ class Sketch:
         # substitute trivial holes
         prism = prism.define_constants(trivial_holes_definitions)
         prism = prism.substitute_constants()
-    
+
         design_space = DesignSpace(holes)
 
         return prism, hole_expressions, design_space
 
- 
+    # refactored
     @classmethod
-    def parse_specification(cls, prism, constant_map, properites_path):
-        # read lines
-        lines = []
-        with open(properites_path) as file:
-            for line in file:
-                line = line.replace(" ", "")
-                line = line.replace("\n", "")
-                if not line or line == "" or line.startswith("//"):
-                    continue
-                lines.append(line)
+    def parse_pc_specification(cls, prism):
 
-        # strip relative error
-        lines_properties = ""
-        relative_error_re = re.compile(r'^(.*)\{(.*?)\}(=\?.*?$)')
-        relative_error_str = None
-        for line in lines:
-            match = relative_error_re.search(line)
-            if match is not None:
-                relative_error_str = match.group(2)
-                line = match.group(1) + match.group(3)
-            lines_properties += line + ";"
-        optimality_epsilon = float(relative_error_str) if relative_error_str is not None else 0
-
-        # parse all properties
+        fs = Specification.string_formulae()
         properties = []
-        optimality_property = None
+        for f in fs:
+            parsed = stormpy.parse_properties_for_prism_program(f, prism)
+            p = PC_Property(parsed)
+            properties.append(p)
 
-        if prism is not None:
-            props = stormpy.parse_properties_for_prism_program(lines_properties, prism)
-        else:
-            props = stormpy.parse_properties_without_context(lines_properties)
-        for prop in props:
-            rf = prop.raw_formula
-            assert rf.has_bound != rf.has_optimality_type, "optimizing formula contains a bound or a comparison formula does not"
-            if rf.has_bound:
-                # comparison formula
-                properties.append(prop)
-            else:
-                # optimality formula
-                assert optimality_property is None, "two optimality formulae specified"
-                optimality_property = prop
-
-        if constant_map is not None:
-            # substitute constants in properties
-            for p in properties:
-                p.raw_formula.substitute(constant_map)
-            if optimality_property is not None:
-                optimality_property.raw_formula.substitute(constant_map)
-
-        # wrap properties
-        properties = [Property(p) for p in properties]
-        if optimality_property is not None:
-            optimality_property = OptimalityProperty(optimality_property, optimality_epsilon)
-
-        specification = Specification(properties,optimality_property)
-        return specification
-    
-
-    def restrict_prism(self, assignment):
-        assert assignment.size == 1
-        substitution = {}
-        for hole_index,hole in enumerate(assignment):
-            ev = self.prism.get_constant(hole.name).expression_variable
-            expr = self.hole_expressions[hole_index][hole.options[0]]
-            substitution[ev] = expr
-        program = self.prism.define_constants(substitution)
-        model = stormpy.build_sparse_model_with_options(program, MarkovChain.builder_options)
-        return model
-
-    
-    
+        s = Specification(properties)
+        return  s
