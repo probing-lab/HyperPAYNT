7801c06862f1cdd6eecafa27942a2c45f40acf75
Francesco Pontiggia <francesco.pontiggia@mail.polimi.it>
2022-06-20 14:50:03 +0200

    Update sketch to parse the hole through the container; remove restrict_prism

--- paynt/paynt/sketch/sketch.py
+++ paynt/paynt/sketch/sketch.py
@@ -38,41 +38,16 @@ class Sketch:
         self.specification = Sketch.parse_pc_specification(self.prism)
         logger.info(f"Found the following specification: {self.specification}")
 
+        # initializing the Model Checking options
+        MarkovChain.initialize(self.specification.stormpy_formulae())
 
-        logger.info("Processing actions...")
-        self.design_space = Sketch.parse_holes(self.prism, self.specification.stormpy_properties())
+        logger.info("Processing actions and Initializing the quotient...")
+        self.quotient = HyperPropertyQuotientContainer(self)
 
         logger.info(f"Sketch has {self.design_space.num_holes} holes")
         logger.info(f"Design space size: {self.design_space.size}")
-
-        # TODO: working from here
-        self.design_space.property_indices = self.specification.all_constraint_indices()
-        MarkovChain.initialize(self.specification.stormpy_formulae())
-
-        logger.info(f"Initializing the quotient ...")
-        self.quotient = HyperPropertyQuotientContainer(self)
-
         logger.info(f"Sketch parsing complete.")
         Profiler.stop()
-
-    @classmethod
-    def parse_holes(cls, program, properties):
-
-        # building the MDP
-        model = stormpy.build_model(program, properties)
-
-        # parse holes (i.e, actions of the model)
-        holes = Holes()
-        for state in model.states:
-            options = []
-            for action in state.actions:
-                options.append(action)
-            if (options.size() > 1):
-                hole = Hole(state, options)
-                holes.append(hole)
-
-        design_space = DesignSpace(holes)
-        return design_space
 
     @classmethod
     def parse_pc_specification(cls, prism):
@@ -83,15 +58,3 @@ class Sketch:
             p = PC_Property(parsed)
             properties.append(p)
         return Specification(properties)
-
-    # TODO: do we need this? ( I have no idea at the moment)
-    def restrict_prism(self, assignment):
-        assert assignment.size == 1
-        substitution = {}
-        for hole_index,hole in enumerate(assignment):
-            ev = self.prism.get_constant(hole.name).expression_variable
-            expr = self.hole_expressions[hole_index][hole.options[0]]
-            substitution[ev] = expr
-        program = self.prism.define_constants(substitution)
-        model = stormpy.build_sparse_model_with_options(program, MarkovChain.builder_options)
-        return model
