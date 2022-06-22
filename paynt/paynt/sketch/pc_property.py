cbfaea57e45780fd3518df3f6729d1bf9f97037c
Francesco Pontiggia <francesco.pontiggia@mail.polimi.it>
2022-06-22 10:34:03 +0200

    refactor pc_property.oy, only one todo

--- paynt/paynt/sketch/pc_property.py
+++ paynt/paynt/sketch/pc_property.py
@@ -15,6 +15,9 @@ logger = logging.getLogger(__name__)
 #   & (P(F die4(s1)) = P(F die4(s2)))
 #   & (P(F die5(s1)) = P(F die5(s2)))
 #   & (P(F die6(s1)) = P(F die6(s2)))
+# these equivalences are converted into (e.g.)
+# (P(F die1(s1)) <= P(F die1(s2))) & (P(F die1(s2)) = P(F die1(s1)))
+
 
 class PC_Property:
     # model checking precision
@@ -25,46 +28,70 @@ class PC_Property:
     ''' Wrapper over a stormpy property. '''
 
     def __init__(self, prop):
-
-        #the state variables of this property
-        self.L_state_var = "s1"
-        self.R_state_var = "s2"
-
-        # each pc_property specifies an equality between two probabilities
+        # TODO: maybe add the state identifier?
         self.property = prop
-        self.op = operator.eq
-        self.formula = prop.raw_formula
+        rf = prop.raw_formula
+
+        # each pc_property specifies an equality between two probabilities,
+        # but we resort to LEQ relations
+        self.minimizing = True
+        self.op = stormpy.ComparisonType.LEQ
+
+        # the threshold is set at every model check query
+        self.threshold = None
+
+        # set optimality type
+        self.formula = rf.clone()
+        self.formula.set_optimality_type(stormpy.OptimizationDirection.Minimize)
+
+        # Construct alternative quantitative formula to use in AR.
+        self.formula_alt = self.formula.clone()
+        optimality_type = stormpy.OptimizationDirection.Maximize
+        self.formula_alt.set_optimality_type(optimality_type)
+
+        self.formula_str = rf
+
+    @property
+    def reward(self):
+        return False
 
     def __str__(self):
-        return str(self.formula) + " = " + str(self.formula)
+        return str(self.formula_str)
 
-    # TODO: refactor this, we don't need it so abstract (and introduce confidence intervals)
+    @staticmethod
+    def above_mc_precision(a, b):
+        return abs(a-b) > PC_Property.mc_precision
+
+    @staticmethod
+    def above_float_precision(a, b):
+        return abs(a-b) > PC_Property.float_precision
+
     def meets_op(self, a, b):
-        return self.op(a, b)
+        return not PC_Property.above_float_precision(a,b) or self.op(a,b)
 
     def satisfies_threshold(self, value_left, value_right):
         return self.meets_op(value_left, value_right)
 
-# a specification is just
-#    - a set of properties (i.e. all the equalities)
-#    - the dictionary to retrive the state quantifications
+
+# a specification is just a set of properties (i.e. all the equalities)
 class Specification:
 
     def __init__(self, constraints):
         self.constraints = constraints
-        # see the formula for the probabilistic conformance
-        self.sq_dict = {"s1" : "AS", "s2" : "ES"}
 
     def __str__(self):
         constraints = ",".join([str(c) for c in self.constraints])
         return f"constraints: {constraints}"
 
+    @property
+    def has_optimality(self):
+        return False
+
     def all_constraint_indices(self):
         return [1,2,3,4,5,6]
 
     def stormpy_properties(self):
-        #Left or Right does not matter, they are the same
-        return [c.L_property for c in self.constraints]
+        return [c.property for c in self.constraints]
 
     @classmethod
     def string_formulae(cls):
@@ -73,7 +100,6 @@ class Specification:
 
 
 # TODO: refactor this as well (I have no idea whether we need it at the moment)
-
 class PropertyResult:
     def __init__(self, prop, result, value_left, value_right):
         self.property = prop
@@ -84,6 +110,7 @@ class PropertyResult:
 
     def __str__(self):
         return str(self.value_left) + " vs " + str(self.value_right)
+
 
 class ConstraintsResult:
     '''
@@ -102,7 +129,6 @@ class ConstraintsResult:
         return ",".join([str(result) for result in self.results])
 
 
-# TODO: refactor this as well ( I have no idea at the moment)
 class MdpPropertyResult:
     def __init__(self,
                  prop, primary, secondary, feasibility,
