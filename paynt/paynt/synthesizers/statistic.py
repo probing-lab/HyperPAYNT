029a7b2aed73a2542dc04daebb2a746ea4ca3a5c
Francesco Pontiggia <francesco.pontiggia@mail.polimi.it>
2022-09-09 18:24:33 +0200

    add more statistics about sat and unsat members

--- paynt/paynt/synthesizers/statistic.py
+++ paynt/paynt/synthesizers/statistic.py
@@ -36,6 +36,8 @@ class Statistic:
 
         self.acc_conflicts = [0 for _ in range(sketch.design_space.num_holes +1)]
         self.avg_conflict_size = 0
+        self.cegis_sat_members = 0
+        self.cegis_unsat_members = 0
 
         self.iterations_mdp = 0
         self.acc_size_mdp = 0
@@ -44,12 +46,11 @@ class Statistic:
         self.acc_decided_families = 0
         self.acc_decided_families_size = 0
         self.avg_decided_families_size = 0
+        self.ar_unsat_members = 0
+        self.ar_sat_members = 0
 
         self.feasible = None
         self.assignment = None
-
-        self.unsat_members = 0
-        self.sat_members = 0
 
         self.synthesis_time = Timer()
         self.status_horizon = Statistic.status_period
@@ -76,15 +77,15 @@ class Statistic:
         self.acc_decided_families += 1
         self.acc_decided_families_size += family.size
         if feasible:
-            self.sat_members += family.size
+            self.ar_sat_members += family.size
         else:
-            self.unsat_members += family.size
+            self.ar_unsat_members += family.size
 
     def add_dtmc_sat_result(self, sat):
         if sat:
-            self.sat_members += 1
+            self.cegis_sat_members += 1
         else:
-            self.unsat_members += 1
+            self.cegis_unsat_members += 1
 
     def status(self):
         fraction_rejected = (self.synthesizer.explored + self.synthesizer.sketch.quotient.discarded) / self.sketch.design_space.size
@@ -140,18 +141,24 @@ class Statistic:
         super_quotient_states = self.sketch.quotient.quotient_mdp.nr_states
         super_quotient_actions = self.sketch.quotient.quotient_mdp.nr_choices
 
+        cegis_sat_stats = f"CEGIS Sat members found: {self.cegis_sat_members}, CEGIS Unsat Members found: {self.cegis_unsat_members}"
+        ar_sat_stats = f"AR Sat members found: {self.ar_sat_members}, AR Unsat Members found: {self.ar_unsat_members}"
+
+
+
         design_space = f"number of holes: {self.sketch.design_space.num_holes}, family size: {self.sketch.design_space.size}, super quotient: {super_quotient_states} states / {super_quotient_actions} actions"
         timing = f"method: {self.synthesizer.method_name}, synthesis time: {round(self.synthesis_time.time, 2)} s"
 
         family_stats = ""
         ar_stats = f"AR stats: avg MDP size: {round(self.avg_size_mdp)}, iterations: {self.iterations_mdp}" \
-                   f", decided families: {self.acc_decided_families}, average decided families size: {self.avg_decided_families_size}"
+                   f", decided families: {self.acc_decided_families}, average decided families size: {self.avg_decided_families_size}" \
+                   f", {ar_sat_stats}"
         conflict_stats = ";\n".join([ f"{counter} conflicts of size {i}" for i, counter in enumerate (self.acc_conflicts)])
         cegis_stats = f"CEGIS stats: avg DTMC size: {round(self.avg_size_dtmc)}, iterations: {self.iterations_dtmc}" \
                       f", conflicts: {sum(self.acc_conflicts)}, average conflict size: {self.avg_conflict_size}\n" \
-                      f"{conflict_stats}"
+                      f"{conflict_stats},\n {cegis_sat_stats}"
 
-        sat_stats = f"Sat members found: {self.sat_members}\nUnsat Members found: {self.unsat_members}"
+
 
         if self.iterations_mdp > 0:
             family_stats += f"{ar_stats}\n"
@@ -166,7 +173,7 @@ class Statistic:
         sep = "--------------------\n"
         summary = f"{sep}Synthesis summary\n" \
                 f"{specification}{disjoint_indexes}\n{timing}\n{design_space}\n{explored}\n" \
-                f"{family_stats}\n{sat_stats}\n{result}\n{assignment}" \
+                f"{family_stats}\n{result}\n{assignment}" \
                 f"{sep}"
         return summary
 
